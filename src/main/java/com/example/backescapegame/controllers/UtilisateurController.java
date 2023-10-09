ccefe6886184e7e03dddc5fa644c29faa9e656ce
debian-kristen <debian-kristen@debian-kristen.kristen>
2023-10-09 17:31:23 +0200

    ufuphfurfrfry

--- src/main/java/com/example/backescapegame/controllers/UtilisateurController.java
+++ src/main/java/com/example/backescapegame/controllers/UtilisateurController.java
@@ -8,19 +8,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/user")
 public class UtilisateurController {
     @Autowired
     private UtilisateurRepository utilisateurRepository;
-    @GetMapping(value="/users")
+    @GetMapping(value="/utilisateurs")
     ResponseEntity<Iterable<Utilisateur>> allUtilisateurs(){
 
         return ResponseEntity.ok(utilisateurRepository.findAll());
     }
-    @PostMapping(value="/createUser")
+    @PostMapping(value="/creerUtilisateur")
     void insertUser(@RequestBody Utilisateur body){
         utilisateurRepository.save(body);
+    }
+    @GetMapping(value = "/scoreMax")
+    Integer scoreMax(){
+        return utilisateurRepository.findMaxScore();
+    }
+    @GetMapping(value = "/utilisateurScoreMax")
+    Utilisateur utilisateurMaxScore(){
+        return utilisateurRepository.findUtilisateurMaxScore();
+    }
+    @GetMapping(value="/scoreUtilisateur/{id}")
+    Optional<Utilisateur> scoreUtilisateur(@PathVariable Integer id){
+        return utilisateurRepository.findById(id);
     }
 
 }
--- src/main/java/com/example/backescapegame/repositories/UtilisateurRepository.java
+++ src/main/java/com/example/backescapegame/repositories/UtilisateurRepository.java
@@ -3,8 +3,17 @@ package com.example.backescapegame.repositories;
 
 
 import com.example.backescapegame.entities.Utilisateur;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UtilisateurRepository extends CrudRepository<Utilisateur, Integer> {
+    @Query("Select max(utilisateur.score) from Utilisateur utilisateur")
+    Integer findMaxScore();
+
+    Optional<Utilisateur> findById(Integer id);
+    @Query("SELECT ut FROM Utilisateur ut WHERE ut.score = (SELECT MAX(u.score) FROM Utilisateur u)")
+    Utilisateur findUtilisateurMaxScore();
 
 }
